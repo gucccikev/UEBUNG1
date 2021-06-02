5d385f83ecce8803b9a9dfb6e13a95a91e46c4b5
gucccikev <kevser.akpinar@stud.fh-campuswien.ac.at>
2021-06-02 02:52:00 +0200

    moreThanThrice

--- src/main/java/at/ac/fhcampuswien/Password.java
+++ src/main/java/at/ac/fhcampuswien/Password.java
@@ -1,9 +1,82 @@
 package at.ac.fhcampuswien;
 import java.lang.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Password {
 
+    private String password;
+
+    public Password(String password){
+        this.password = password;
+    }
+
+    public boolean validLength(){
+        return password.length() >= 8 && password.length() <= 25;
+    }
+
+    public boolean upLowCase(){
+        boolean isUp = false;
+        boolean isLow = false;
+
+        for(int i = 0; i < password.length(); i++){
+            if(Character.isUpperCase(password.charAt(i))){
+                isUp = true;
+            }
+            if(Character.isLowerCase(password.charAt(i))){
+                isLow = true;
+            }
+            if(isLow && isUp){
+                return true; // if condition true, no need to loop once again
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNumbers(){
+
+        for(int i = 0; i < password.length(); i++){
+            if(password.matches(".*\\d.*")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsSymbol(){
+         return (password.matches(".*[()#$?!%/@].*"));
+    }
+
+
+    public boolean notTwoNum(){
+        boolean notTwo = false;
+        int counter = 0;
+        char previous = password.charAt(0);
+
+        for(int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) {
+                if (previous + 1 == c) {
+                    counter++;
+                } else {
+                    counter = 1;
+                }
+                if (counter >= 3) {
+                    return false;
+                }
+                previous = c;
+            }
+        }
+        return true;
+    }
 
 
 
+    public String showIfValid(boolean passwordValid){
+        if(passwordValid){
+            return "Valid password";
+        } else {
+            return "Not a valid password";
+        }
+    }
 }
--- src/test/java/at/ac/fhcampuswien/PasswordTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordTest.java
@@ -9,9 +9,49 @@ public class PasswordTest {
     @Test
     @DisplayName("Does password have valid length?")
     public void validLengthTest(){
-        Password p = new Password();
+        Password p = new Password("hello1234");
         boolean actual = p.validLength();
         assertTrue(actual);
     }
 
+
+    @Test
+    @DisplayName("Has to have upper and lowercase")
+    public void upLowCaseTest(){
+        Password p = new Password("OoOOOOo");
+        boolean actual = p.upLowCase();
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Has to have numbers")
+    public void containsNumbersTest(){
+        Password p = new Password("hgd789");
+        boolean actual = p.containsNumbers();
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Has minimum one of the symbols")
+    public void containsSymbolTest(){
+        Password p = new Password("sam#");
+        boolean actual = p.containsSymbol();
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("More than two numbers (right after) not allowed")
+    public void notTwoNumTest(){
+        Password p = new Password("7ig1abc123");
+        boolean actual = p.notTwoNum();
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Same number more than thrice not allowed")
+    public void moreThanThriceTest(){
+        Password p = new Password("");
+        boolean actual = p.moreThanThrice();
+        assertTrue(actual);
+    }
 }
